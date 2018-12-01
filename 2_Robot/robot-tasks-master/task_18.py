7c92625b14da187f89a4e4a1253e5965a955f09c
Saf4ik <Saf4ik85@gmail.com>
2018-12-01 16:42:41 +0300

    Решена задача 18

--- 2_Robot/robot-tasks-master/task_18.py
+++ 2_Robot/robot-tasks-master/task_18.py
@@ -2,10 +2,26 @@
 
 from pyrob.api import *
 
+def finish():
+    while not wall_is_above():
+        move_up()
+    while not wall_is_on_the_left():
+        move_left()
 
 @task
 def task_8_28():
-    pass
+    while wall_is_above() and not wall_is_on_the_left():
+        move_left()
+    if not wall_is_above():
+        finish()
+    else:
+        while wall_is_above():
+            move_right()
+        finish()
+
+
+
+
 
 
 if __name__ == '__main__':
