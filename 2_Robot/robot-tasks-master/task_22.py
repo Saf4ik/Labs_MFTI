3ccefb715d745c016c6861f28701bb1942fd41a6
Saf4ik <Saf4ik85@gmail.com>
2018-12-01 18:22:48 +0300

    Решена задача 22

--- 2_Robot/robot-tasks-master/task_22.py
+++ 2_Robot/robot-tasks-master/task_22.py
@@ -5,8 +5,31 @@ from pyrob.api import *
 
 @task
 def task_5_10():
-    pass
+    # проверка на одноклеточное поле
+    if wall_is_beneath and wall_is_on_the_right():
+        fill_cell()
 
+    while not wall_is_beneath():
+        while not wall_is_on_the_right():
+            fill_cell()
+            move_right()
+        fill_cell()
+        if not wall_is_beneath():
+            move_down()
+        while not wall_is_on_the_left():
+            fill_cell()
+            move_left()
+        fill_cell()
+        if not wall_is_beneath():
+            move_down()
+
+            if wall_is_beneath():
+                while not wall_is_on_the_right():
+                    fill_cell()
+                    move_right()
+                fill_cell()
+                while not wall_is_on_the_left():
+                    move_left()
 
 if __name__ == '__main__':
     run_tasks()
