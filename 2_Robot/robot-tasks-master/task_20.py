0d9d19d23e731bbacbb68196f0cf0764839705ae
Saf4ik <Saf4ik85@gmail.com>
2018-12-01 17:06:30 +0300

    Решена задача 20

--- 2_Robot/robot-tasks-master/task_20.py
+++ 2_Robot/robot-tasks-master/task_20.py
@@ -5,8 +5,18 @@ from pyrob.api import *
 
 @task(delay=0.05)
 def task_4_3():
-    pass
-
+    for k in range(6):
+        for k in range(27):
+            move_right()
+            fill_cell()
+        move_right()
+        move_down()
+        for k in range(27):
+            move_left()
+            fill_cell()
+        move_left()
+        move_down()
+    move_right()
 
 if __name__ == '__main__':
     run_tasks()
