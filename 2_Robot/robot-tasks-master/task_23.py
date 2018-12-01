24176328b4ad6fbb63eb67c1ea679135557dedfc
Saf4ik <Saf4ik85@gmail.com>
2018-12-01 18:58:36 +0300

    Решена задача 23

--- 2_Robot/robot-tasks-master/task_23.py
+++ 2_Robot/robot-tasks-master/task_23.py
@@ -2,10 +2,27 @@
 
 from pyrob.api import *
 
+def paint():
+    while not wall_is_above():
+        move_up()
+        fill_cell()
+    while not wall_is_beneath():
+        move_down()
+
 
 @task(delay=0.01)
 def task_6_6():
-    pass
+    move_right()
+    while not wall_is_on_the_right():
+        if not wall_is_above():
+            paint()
+            move_right()
+        else:
+            move_right()
+    if not wall_is_above():
+        paint()
+    while wall_is_beneath():
+        move_left()
 
 
 if __name__ == '__main__':
