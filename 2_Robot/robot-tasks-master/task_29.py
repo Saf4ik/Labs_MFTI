49bd4fec2be12a4eeeb1e973961536441d8e7c4d
Saf4ik <Saf4ik85@gmail.com>
2018-12-02 15:27:45 +0300

    Решена задача 29

--- 2_Robot/robot-tasks-master/task_29.py
+++ 2_Robot/robot-tasks-master/task_29.py
@@ -5,7 +5,21 @@ from pyrob.api import *
 
 @task
 def task_7_7():
-    pass
+
+    flag = 0
+
+    while not wall_is_on_the_right():
+        if cell_is_filled():
+            flag +=1
+            if flag < 3:
+                move_right()
+            else:
+                break
+        else:
+            flag = 0
+            if not wall_is_on_the_right():
+                move_right()
+
 
 
 if __name__ == '__main__':
