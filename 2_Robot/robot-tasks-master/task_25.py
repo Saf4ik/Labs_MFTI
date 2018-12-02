478978808931deb7d450dc3025a842c9f20741db
Saf4ik <Saf4ik85@gmail.com>
2018-12-02 14:02:29 +0300

    Решена задача 25

--- 2_Robot/robot-tasks-master/task_25.py
+++ 2_Robot/robot-tasks-master/task_25.py
@@ -2,11 +2,29 @@
 
 from pyrob.api import *
 
+def cross():
+    fill_cell()
+    move_right()
+    fill_cell()
+    move_down()
+    fill_cell()
+    move_right()
+    move_up()
+    fill_cell()
+    move_up()
+    move_left()
+    fill_cell()
+
 
 @task
 def task_2_2():
-    pass
-
+    move_down(2)
+    for i in range(4):
+        cross()
+        move_right(3)
+        move_down()
+    cross()
+    move_left()
 
 if __name__ == '__main__':
     run_tasks()
