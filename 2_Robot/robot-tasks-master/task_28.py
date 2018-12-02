97a4225346dc635e2eb82be34d93d48c84c281b3
Saf4ik <Saf4ik85@gmail.com>
2018-12-02 15:02:16 +0300

    Решена задача 28

--- 2_Robot/robot-tasks-master/task_28.py
+++ 2_Robot/robot-tasks-master/task_28.py
@@ -5,7 +5,12 @@ from pyrob.api import *
 
 @task
 def task_7_6():
-    pass
+    n = 1
+    while n <= 5:
+        move_right()
+        if cell_is_filled():
+            n += 1
+
 
 
 if __name__ == '__main__':
