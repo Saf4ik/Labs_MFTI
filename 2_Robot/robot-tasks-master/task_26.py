a0824f3cf5e88fea7754997a6d0176eceb5d3a3a
Saf4ik <Saf4ik85@gmail.com>
2018-12-02 14:42:23 +0300

    Решена задача 26

--- 2_Robot/robot-tasks-master/task_26.py
+++ 2_Robot/robot-tasks-master/task_26.py
@@ -2,10 +2,42 @@
 
 from pyrob.api import *
 
+def cross():
+    fill_cell()
+    move_right()
+    fill_cell()
+    move_down()
+    fill_cell()
+    move_right()
+    move_up()
+    fill_cell()
+    move_up()
+    move_left()
+    fill_cell()
+    move_right()
 
-@task(delay=0.02)
+@task(delay=0.01)
 def task_2_4():
-    pass
+
+
+    move_down()
+    while not wall_is_on_the_right():
+        cross()
+        while not wall_is_on_the_right():
+            move_right(2)
+            move_down()
+            cross()
+        move_down(2)
+        if wall_is_beneath():
+            while not wall_is_on_the_left():
+                move_left()
+            move_up(2)
+            break
+        else:
+            move_down(3)
+            while not wall_is_on_the_left():
+                move_left()
+
 
 
 if __name__ == '__main__':
